42d1bb228134377bf86804988e14ddb9d0a46d25
Aman Rastogi <aman_1901cs05@iitp.ac.in>
2019-12-19 22:15:33 +0530

    [FEAT]: Contact Us
    [FEAT]: Contact Us
    
    Required Changes Done
    
    Corrected Code
    
    Corrected Code

--- functions/utility.php
+++ functions/utility.php
@@ -27,7 +27,7 @@ function rollno_exists($rollno){
 	}
 }
 
-function send_email($email,$subject,$msg){
+function send_email($email,$subject,$msg, $replyto = 'hayyoulistentome@gmail.com'){
 // 	return (mail($email,$subject,$msg,$headers));
 
 	require 'PHPMailerAutoload.php';
@@ -47,7 +47,7 @@ function send_email($email,$subject,$msg){
 	$mail->setFrom('hayyoulistentome@gmail.com', 'SAAR IIT Patna');
 	$mail->addAddress($email);     // Add a recipient
 	//$mail->addAddress('ellen@example.com');               // Name is optional
-	$mail->addReplyTo('hayyoulistentome@gmail.com', 'Information');
+	$mail->addReplyTo($replyto, 'Information');
 	//$mail->addCC('cc@example.com');
 	//$mail->addBCC('bcc@example.com');
 
@@ -68,3 +68,10 @@ function send_email($email,$subject,$msg){
 	    return true;
 	}
 }
+
+	function test_input($data) {
+		$data = trim($data);
+		$data = stripslashes($data);
+		$data = htmlspecialchars($data);
+		return $data;
+	}
