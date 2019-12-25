4e786228f4e7d033d9c5031c744887cc2d283dd8
pranaykgupta <pranaykgupta21@gmail.com>
2019-12-25 16:10:00 +0530

    enhanced get post api

--- functions/getPosts.php
+++ functions/getPosts.php
@@ -1,31 +1,37 @@
 <?php
     include('init.php');
 
-    // Initialising the variables
-    $response = array();
-    $data = array();
-    $post = array();
+    if(isset($_GET['limit']) && isset($_GET['offset'])){
 
-    $sql = "SELECT `id`,`user_id`, `cat_id`, `post_time`, `title`, `body`, `upvotes_ids`, `downvotes_ids` FROM `posts` WHERE 1";
-    $result = query($sql);
-    if(row_count($result) == 0){
-        $response['status'] = 409;
-    }else{
-        $response['status'] = 210;
-        while($row = fetch_array($result)){
-            $post['id'] = $row['id'];
-            $post['user_id'] = $row['user_id'];
-            $post['cat_id'] = $row['cat_id'];
-            $post['post_time'] = $row['post_time'];
-            $post['title'] = $row['title'];
-            $post['body'] = $row['body'];
-            $post['upvotes'] = count(unserialize($row['upvotes_ids']));
-            $post['downvotes'] = count(unserialize($row['downvotes_ids']));
-            $data[] = $post;
-        }   
+        // Initialising the variables
+        $response = array();
+        $data = array();
+        $post = array();
+        $offset = $_GET['offset'];
+        $limit = $_GET['limit'];
+
+        $sql = "SELECT `id`,`user_id`, `cat_id`, `post_time`, `title`, `body`, `upvotes_ids`, `downvotes_ids` FROM `posts` LIMIT $offset,$limit";
+        $result = query($sql);
+        if(row_count($result) == 0){
+            $response['status'] = 409;
+        }else{
+            $response['status'] = 210;
+            while($row = fetch_array($result)){
+                $post['id'] = $row['id'];
+                $post['user_id'] = $row['user_id'];
+                $post['cat_id'] = $row['cat_id'];
+                $post['post_time'] = $row['post_time'];
+                $post['title'] = $row['title'];
+                $post['body'] = substr($row['body'],0,50);
+                $post['upvotes'] = count(unserialize($row['upvotes_ids']));
+                $post['downvotes'] = count(unserialize($row['downvotes_ids']));
+                $data[] = $post;
+            }   
+            
+        }
         $response['data'] = $data;
+        echo json_encode($response);
     }
     
-    echo json_encode($response);
 
 ?>
